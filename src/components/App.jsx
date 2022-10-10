4a669385f9d9fd8dee9a4073e9dd2c277adab01f
PavloPSh <pavlopshe@gmail.com>
2022-10-11 02:16:53 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import user from './profile/user';
+import data from './stats/data';
+import friends from './friendList/friends'
+
+import { Profile } from './profile/Profile';
+import { Statistics } from './stats/Statistics';
+import { FriendList } from './friendList/FriendList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} 
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
+      
     </div>
   );
 };
+
+
---
+++ src/components/stats/Statistics.jsx
@@ -0,0 +1,20 @@
+
+export const Statistics = ({ title, stats }) => {
+    return (
+        <div>
+            {title && (
+                <h2>{title}</h2>
+            )}
+            <ul>
+                {
+                    stats.map(({ id, label, percentage }) => 
+                        <li key={id}>
+                            <span>{ label }</span>
+                            <span>{ percentage }%</span>
+                        </li>)
+                }
+            </ul>
+
+        </div> 
+    )
+}
