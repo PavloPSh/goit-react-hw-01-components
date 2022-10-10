2bbc05480f3213311e765d442c208a154386f63f
PavloPSh <pavlopshe@gmail.com>
2022-10-11 02:25:12 +0300

    dep

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from './profile/user';
 import data from './stats/data';
 import friends from './friendList/friends'
+import transactions from './transaction/transactions'
 
 import { Profile } from './profile/Profile';
 import { Statistics } from './stats/Statistics';
 import { FriendList } from './friendList/FriendList';
+import { TransactionHistory } from './transaction/TransactionHistory';
 
 export const App = () => {
   return (
@@ -20,6 +22,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory />
       
     </div>
   );
